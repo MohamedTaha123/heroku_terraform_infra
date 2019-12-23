32bc30ce49a506057dfc9150f5e327c65d129ec9
taha <mohamedtaha.boutayeb@gmail.com>
2019-12-23 02:48:27 -0100

    chore: add more features

--- providers/heroku/main.tf
+++ providers/heroku/main.tf
@@ -14,3 +14,26 @@ resource "heroku_app" "default" {
     "heroku/ruby"
   ]
 }
+
+resource "heroku_build" "default" {
+  app = "${heroku_app.default.name}"
+  buildpacks = ["https://github.com/heroku/heroku-buildpack-ruby.git"]
+  source = {
+    url = "https://github.com/MohamedTaha123/rails_heroku_terraform/archive/0.0.3.tar.gz"
+    version = "0.0.3"
+  }
+ 
+}
+
+# Launch the app's web process by scaling-up
+resource "heroku_formation" "default" {
+  app        = "${heroku_app.default.name}"
+  type       = "web"
+  quantity   = 1
+  size       = "Standard-1x"
+  depends_on = ["heroku_build.default"]
+}
+
+output "example_app_url" {
+  value = "https://${heroku_app.default.name}.herokuapp.com"
+}
